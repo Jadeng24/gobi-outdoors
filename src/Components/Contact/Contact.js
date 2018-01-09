7a7b0f66cc437fe35273dbd903016fcdbe6af1c2
Jaden Goodwin <jaden.goodwin24@gmail.com>
2018-01-09 10:39:52 -0700

    more on contact page

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -13,8 +13,14 @@ class Contact extends Component {
     render() {
         return (
             <content className='Contact'>
-                <h1> CONTACT </h1>
-                    {/* <h4>Send me a message </h4> */}
+                <h1 className='ContactTitle'> CONTACT </h1>
+                {/* <h4>Send me a message </h4> */}
+
+                <div className='ContactInfo'>
+                    <div className='leftContact'>
+                        <h2>titlelsd</h2>
+                        <p>asd asdf asl;kasd lkas dflkjasd fljasd flkjasd flkjasd fl;ajsdf l;as dlkjasdlfjasdflkjasdflkasdf;l</p>
+                    </div>
                     <form>
 
 
@@ -23,8 +29,11 @@ class Contact extends Component {
                         <h3 >Email Address *</h3><input type='text' maxLength='30' onChange={(e) => this.handleChange('email', e.target.value)} />
                         <h3>Subject</h3><input type='text' maxLength='40' onChange={(e) => this.handleChange('subject', e.target.value)} />
                         <h3>Message *</h3><textarea type='text' rows='5' cols='10' placeholder='Send me a message' maxLength='300' onChange={(e) => this.handleChange('message', e.target.value)}></textarea>
+
+                        <div  className='sendMessageBtn' onClick={this.sendEmail}><h3>Send</h3></div >
                     </form>
-                    <button className='sendMessageBtn' onClick={this.sendEmail}><h3>Send</h3></button>
+                    
+                </div>
             </content>
         )
     }
